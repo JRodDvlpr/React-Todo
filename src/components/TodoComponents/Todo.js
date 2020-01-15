ffa638898a105881390bc1d38e9b51c34af71a7d
JRodDvlpr <Mialienproductions@gmail.com>
2020-01-14 23:43:37 -0500

    todolist and todo boiler

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,1 +1,8 @@
 import React from 'react';
+
+export class Todo extends React.Component {
+
+    return;
+}
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,3 +1,12 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+
 import React from 'react';
+
+export class TodoList extends React.Component {
+
+    return;
+}
+
+export default TodoList;
